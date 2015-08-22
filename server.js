3706060cd3339a57e5e0079b19fa86b5e8556ab9
Vladimir Galkin <vladgalkin@gmail.com>
2015-08-22 15:22:56 +0300

    Add routes and query string parameters parsing to retrieve questions by id and all questions in specific state (answered | not answered | all)

--- server.js
+++ server.js
@@ -6,6 +6,59 @@ var app = express();
 
 var router = express.Router();
 
+var QuestionType = {
+	All : -1,
+	NotAnswered : 0,
+	Answered: 1,
+};
+
+router.get('/questions', function(request, response) {
+	var questionType = QuestionType.All;
+
+	if (request.query.isAnswered) {
+		var answeredValue = request.query.isAnswered;
+
+		switch (answeredValue.toLowerCase()) {
+			case "yes":
+				questionType = QuestionType.Answered;
+				break;
+
+			case "no":
+				questionType = QuestionType.NotAnswered;
+				break;
+
+			default:
+				response.sendStatus(400);
+		}
+	}
+
+	console.log('Retrieving questions with status ' + questionType);
+
+	response.sendStatus(200);
+});
+
+router.get('/questions/:questionId', function(request, response) {
+	var questionId = request.params.questionId;
+
+	console.log('Retrieving question with id ' + questionId);
+
+	response.sendStatus(200);
+});
+
+router.post('/questions', function(request, response) {
+	console.log('Posting a question');
+
+	response.sendStatus(201);
+});
+
+router.post('/questions/:questionId/answers', function(request, response) {
+	var questionId = request.params.questionId;
+
+	console.log('Posting an answer for question with id ' + questionId);
+
+	response.sendStatus(201);
+});
+
 router.get('/', function(request, response) {
 	response.json({foo: 'bar'});
 	console.log('Got an API request!');
