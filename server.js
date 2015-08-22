00d257cdc03fc52481857f9797a075ef0c7ec250
Vladimir Galkin <vladgalkin@gmail.com>
2015-08-22 16:27:54 +0300

    Add Express middleware to parse paging query string params

--- server.js
+++ server.js
@@ -6,12 +6,45 @@ var app = express();
 
 var router = express.Router();
 
+var AppDefaults = {
+	PageNo : 1,
+	PageSize : 10
+};
+
 var QuestionType = {
 	All : -1,
 	NotAnswered : 0,
 	Answered: 1,
 };
 
+function isPositiveInt(value) {
+	return /^([1-9]\d*)$/.test(value);
+}
+
+function parsePagingParams(request, response, next) {
+	request.pageNo = AppDefaults.PageNo;
+	request.pageSize = AppDefaults.PageSize;
+
+	if (request.query.pageNo) {
+		if (isPositiveInt(request.query.pageNo)) {
+			request.pageNo = request.query.pageNo;
+		} else {
+			response.sendStatus(400);
+		}
+	}
+
+	if (request.query.pageSize) {
+		if (isPositiveInt(request.query.pageSize)) {
+			request.pageSize = request.query.pageSize;
+		} else {
+			response.sendStatus(400);
+		}
+	}
+
+	next();
+}
+
+router.get('/questions', parsePagingParams);
 router.get('/questions', function(request, response) {
 	var questionType = QuestionType.All;
 
@@ -32,15 +65,20 @@ router.get('/questions', function(request, response) {
 		}
 	}
 
-	console.log('Retrieving questions with status ' + questionType);
+	console.log('Retrieving questions with status ' + questionType
+		+ ' for page number ' + request.pageNo + " page size is "
+		+ request.pageSize);
 
 	response.sendStatus(200);
 });
 
+router.get('/questions/:questionId', parsePagingParams);
 router.get('/questions/:questionId', function(request, response) {
 	var questionId = request.params.questionId;
 
-	console.log('Retrieving question with id ' + questionId);
+	console.log('Retrieving question with id ' + questionId 
+		+ ' for page number ' + request.pageNo + " page size is "
+		+ request.pageSize);
 
 	response.sendStatus(200);
 });
