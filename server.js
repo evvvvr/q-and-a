5a8e65b3bfcebef6c9aabb311a6d873a0d60cc6e
Vladimir Galkin <vladgalkin@gmail.com>
2015-08-23 13:36:54 +0300

    Add sqlite database initialization code.

--- server.js
+++ server.js
@@ -1,73 +1,93 @@
 'use strict';
 
+var fs = require('fs');
+var sqlite3 = require('sqlite3');
 var express = require('express');
 var AppDefaults = require('./app-defaults.js');
 var middleware = require('./middleware.js');
 var questions = require('./questions.js');
 
-var port = process.env.PORT || AppDefaults.Port;
+console.info('Initalizing database.');
 
-var app = express();
+var db = new sqlite3.Database('data.db');
+var dbCreationScript = fs.readFileSync('./db/create.sql', 'utf8');
 
-var router = express.Router();
-
-router.get('/questions', [middleware.parsePagingParams, function(request, response) {
-	var questionType = questions.QuestionType.All;
-
-	if (request.query.isAnswered) {
-		try {
-			questionType = questions.QuestionType.parse(
-				request.query.isAnswered);			
-		} catch (err) {
-			response.sendStatus(404);
-		}
+db.exec(dbCreationScript, function (err) {
+	if (err) {
+		throw err;
 	}
 
-	console.log('Retrieving questions with status ' + questionType
-		+ ' for page number ' + request.pageNo + " page size is "
-		+ request.pageSize);
+	console.info('Database initialized.');
+	db.close();
 
-	response.sendStatus(200);
-}]);
-
-router.post('/questions', function(request, response) {
-	console.log('Posting a question');
-
-	response.sendStatus(201);
+	startApp();
 });
 
-router.get('/questions/:questionId(\\d+)', [middleware.parsePagingParams,
-	function(request, response) {
-		var questionId = request.params.questionId;
+function startApp() {
+	var port = process.env.PORT || AppDefaults.Port;
 
-		console.log('Retrieving question with id ' + questionId 
+	var app = express();
+
+	var router = express.Router();
+
+	router.get('/questions', [middleware.parsePagingParams, function(request, response) {
+		var questionType = questions.QuestionType.All;
+
+		if (request.query.isAnswered) {
+			try {
+				questionType = questions.QuestionType.parse(
+					request.query.isAnswered);			
+			} catch (err) {
+				response.sendStatus(404);
+			}
+		}
+
+		console.log('Retrieving questions with status ' + questionType
 			+ ' for page number ' + request.pageNo + " page size is "
 			+ request.pageSize);
 
 		response.sendStatus(200);
-}]);
+	}]);
 
-router.get('/questions/:questionId(\\d+)/answers', [middleware.parsePagingParams,
-	function(request, response) {
+	router.post('/questions', function(request, response) {
+		console.log('Posting a question');
+
+		response.sendStatus(201);
+	});
+
+	router.get('/questions/:questionId(\\d+)', [middleware.parsePagingParams,
+		function(request, response) {
+			var questionId = request.params.questionId;
+
+			console.log('Retrieving question with id ' + questionId 
+				+ ' for page number ' + request.pageNo + " page size is "
+				+ request.pageSize);
+
+			response.sendStatus(200);
+	}]);
+
+	router.get('/questions/:questionId(\\d+)/answers', [middleware.parsePagingParams,
+		function(request, response) {
+			var questionId = request.params.questionId;
+
+			console.log('Retrieving answers for question with id ' + questionId 
+				+ ' for page number ' + request.pageNo + " page size is "
+				+ request.pageSize);
+
+			response.sendStatus(200);
+	}]);
+
+	router.post('/questions/:questionId(\\d+)/answers', function(request, response) {
 		var questionId = request.params.questionId;
 
-		console.log('Retrieving answers for question with id ' + questionId 
-			+ ' for page number ' + request.pageNo + " page size is "
-			+ request.pageSize);
+		console.log('Posting an answer for question with id ' + questionId);
 
-		response.sendStatus(200);
-}]);
+		response.sendStatus(201);
+	});
 
-router.post('/questions/:questionId(\\d+)/answers', function(request, response) {
-	var questionId = request.params.questionId;
+	app.use(express.static('content'));
+	app.use('/api', router);
 
-	console.log('Posting an answer for question with id ' + questionId);
-
-	response.sendStatus(201);
-});
-
-app.use(express.static('content'));
-app.use('/api', router);
-
-app.listen(port);
-console.log('Listening on port ' + port);
+	app.listen(port);
+	console.log('Listening on port ' + port);
+}
