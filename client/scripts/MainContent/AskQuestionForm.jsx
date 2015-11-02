600d56d2118df5b2ce4846dd10d3395bba3565fd
Vladimir Galkin <vladgalkin@gmail.com>
2015-11-02 17:15:04 +0200

    Add static version of 'Ask Question' React component

--- client/scripts/MainContent/AskQuestionForm.jsx
+++ client/scripts/MainContent/AskQuestionForm.jsx
@@ -2,6 +2,28 @@ import React from 'react';
 
 export default class AskQuestionForm extends React.Component {
     render() {
-        return <span>Ask Question</span>;
+        return (
+            <form className="pure-form pure-form-stacked">
+                <fieldset>
+                    <legend>Ask Your Question</legend>
+                    <input
+                        className="pure-input-2-3"
+                        type="text"
+                        name="userName"
+                        placeholder="Your name"
+                    />
+                    <textarea
+                        className="pure-input-2-3 textarea-text"
+                        name="answerText"
+                        placeholder="Your Question"
+                    />
+                    <input
+                        className="pure-button pure-button-primary"
+                        type="submit"
+                        value="Post Your Question"
+                    />
+                </fieldset>
+            </form>
+        );
     }
 }
