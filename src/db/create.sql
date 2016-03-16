775d6086fbfa946cdd39a4de7258496c0f801d8a
Vladimir Galkin <vladgalkin@gmail.com>
2016-03-16 10:49:10 +0200

    Modify database creation script for PostgreSQL

--- src/db/create.sql
+++ src/db/create.sql
@@ -1,20 +1,25 @@
 Begin;
 
-Create Table If Not Exists Users
-    (Id Integer Primary Key Autoincrement,Login Varchar(255) Not Null Collate NoCase);
+Create Table If Not Exists Users (
+    Id Serial Primary Key,
+    Login citext Check(length(Login) < 255 And length(Login) > 0) Not Null
+);
 
-Create Unique Index If Not Exists User_Login On Users (Login);
+Create Unique Index If Not Exists User_Unique_Login On Users (Login);
 
-Create Table If Not Exists Questions
-    (Id Integer Primary Key Autoincrement, Text Varchar(3000) Not Null,
-        DateTimeAsked Varchar(500) Not Null, UserAsked Integer Not Null,
-        Foreign Key (UserAsked) References Users(Id) On Delete Cascade);
+Create Table If Not Exists Questions (
+    Id Serial Primary Key,
+    Text Text Check(length(Text) > 0 AND length(Text) < 3000) Not Null,
+    DateTimeAsked Timestamp With Time Zone Not Null,
+    UserAsked Integer References Users(Id) On Delete Cascade
+);
 
-Create Table If Not Exists Answers
-    (Id Integer Primary Key Autoincrement, Text Varchar(3000) Not Null,
-        DateTimeAnswered Varchar(500) Not Null,
-        QuestionId Integer Not Null, UserAnswered Integer Not Null,
-        Foreign Key (QuestionId) References Questions(Id) On Delete Cascade,
-        Foreign Key (UserAnswered) References Users(Id) On Delete Cascade);
+Create Table If Not Exists Answers (
+        Id Serial Primary Key,
+        Text Text Check(length(Text) > 0 AND length(Text) < 3000) Not Null,
+        DateTimeAnswered Timestamp With Time Zone Not Null,
+        QuestionId Integer References Questions(Id) On Delete Cascade,
+        UserAnswered Integer References Users(Id) On Delete Cascade
+);
 
 Commit;
